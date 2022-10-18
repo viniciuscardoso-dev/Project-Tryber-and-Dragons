1822b41a38c1fff6a2227018240b44424b281485
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-18 18:43:18 -0300

    fix: atributos special e cost de Archetype

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,15 +1,32 @@
 import { EnergyType } from '../Energy';
 
 export default abstract class Archetype {
+  readonly special: number;
+  readonly cost: number;
+  
   constructor(
-    readonly name: string,
-    readonly special = 0,
-    readonly cost = 0,
-  ) {}
+    private _name: string,
+  ) {
+    this.special = 0;
+    this.cost = 0;
+  }
+  
+  public get name(): string {
+    return this._name;
+  }
+
+  //  tratamento para os atributos private
+  // public get special(): number {
+  //   return this._special;
+  // }
+
+  // public get cost(): number {
+  //   return this._cost;
+  // }
 
   static createdArchetypeInstances(): number {
     throw new Error('Not implemented');  
   }
 
-  abstract get energyType(): EnergyType;
+  public abstract get energyType(): EnergyType;
 }
