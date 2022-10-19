2485df67dd9c93d9104b08ddcd8df70e130d0a56
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-19 13:19:59 -0300

    feat: method levelUp

--- src/Character/Character.ts
+++ src/Character/Character.ts
@@ -5,39 +5,39 @@ import Race, { Elf } from '../Races';
 import getRandomInt from '../utils';
 
 export default class Character implements Fighter {
-  readonly dexterity: number;
-  readonly race!: Race;
+  private _race!: Race;
+  private _archtype!: Archetype;
+  private _energy!: Energy;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _lifePoints: number;
+  private _maxLifePoints: number;
   name: string;
-  readonly archtype!: Archetype;
-  maxLifePoints: number;
-  private lifePoints: number;
-  readonly strength: number;
-  readonly defense: number;
-  readonly energy!: Energy;
 
   constructor(
     name: string,
   ) {
-    this.dexterity = getRandomInt(1, 10);
+    this._dexterity = getRandomInt(1, 10);
     this.name = name;
-    this.race = new Elf(this.name, this.dexterity);
-    this.archtype = new Mage(this.name);
-    this.maxLifePoints = this.race.maxLifePoints / 2;
-    this.lifePoints = this.maxLifePoints;
-    this.strength = getRandomInt(1, 10);
-    this.defense = getRandomInt(1, 10);
-    this.energy.type_ = this.archtype.energyType;
-    this.energy.amount = getRandomInt(1, 10); 
+    this._race = new Elf(this.name, this._dexterity);
+    this._archtype = new Mage(this.name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy.type_ = this._archtype.energyType;
+    this._energy.amount = getRandomInt(1, 10); 
   }
 
   receiveDamage(attackPoints: number): number {
-    const damage = this.defense - attackPoints;
+    const damage = this._defense - attackPoints;
     if (damage > 0) {
-      const newLifePoints = this.lifePoints - damage;
+      const newLifePoints = this._lifePoints - damage;
       if (newLifePoints <= 0) {
-        this.lifePoints = -1;
+        this._lifePoints = -1;
       } else {
-        this.lifePoints = newLifePoints; 
+        this._lifePoints = newLifePoints; 
       }
       return newLifePoints as number;
     }
@@ -45,6 +45,35 @@ export default class Character implements Fighter {
   }
 
   attack(enemy: Fighter): void {
-    enemy.receiveDamage(this.strength);
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._strength += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    const newMaxLife = this._maxLifePoints + getRandomInt(1, 10);
+    if (newMaxLife > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+      this._lifePoints = this._race.maxLifePoints;
+    } else {
+      this._maxLifePoints = newMaxLife;
+      this._lifePoints = newMaxLife;
+    }
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
   }
 }
+
+// problema com o energy e seu modificador de acesso por ser um objeto e
