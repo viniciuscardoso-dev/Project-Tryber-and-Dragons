6adf300b60c634e34927b6121324c9852905698c
vinicius cardoso <cardosovinicius87@gmail.com>
2022-10-20 09:19:47 -0300

    fix: attack Simple Fighter

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -14,7 +14,6 @@ export default class PVE extends Battle {
       while (this.player.lifePoints > 0 && m.lifePoints > 0) {
         m.attack(this.player);
         this.player.attack(m);
-        console.log(this.player);
       }
     });
     return super.fight();
--- src/Fighter/SimpleFighter.ts
+++ src/Fighter/SimpleFighter.ts
@@ -1,6 +1,8 @@
+import Fighter from './Fighter';
+
 export default interface SimpleFighter {
   lifePoints: number,
   strength: number,
-  attack(enemy: SimpleFighter): void,
+  attack(enemy: SimpleFighter | Fighter): void,
   receiveDamage(attackPoints: number): number, 
 }
